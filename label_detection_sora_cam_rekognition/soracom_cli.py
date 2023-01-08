54ffbc2fa8170e989221c7febde3c093aae6ee06
Naoki Mikuni <nmikuni@outlook.com>
2023-01-08 12:34:43 +0900

    Prettier

--- label_detection_sora_cam_rekognition/soracom_cli.py
+++ label_detection_sora_cam_rekognition/soracom_cli.py
@@ -12,22 +12,22 @@ def run_cmd(cmd, arg):
 def export_sora_cam_image_url(device_id, arg):
     """
     Return image URL exported from Soracom Cloud Camera Service.
-    The program uses soracom-cli
+    The program uses soracom-cli.
     If the code run in Lambda, the soracom-cli should be used by Lambda layer.
     """
     photo_shoot_time = int(time.time()) * 1000
     export_sora_cam_image_cmd = "soracom sora-cam devices images export" \
-        + " --image-filters 'wide_angle_correction' --device-id " \
-        + device_id + " --time " + str(photo_shoot_time)
+        + " --image-filters 'wide_angle_correction'" \
+        + " --device-id " + device_id + " --time " + str(photo_shoot_time)
     exported_image_info = json.loads(
         run_cmd(cmd=export_sora_cam_image_cmd, arg=arg).stdout.decode())
     export_id = exported_image_info.get("exportId")
 
     # As the export takes time, need to sleep here
     time.sleep(2)
-    get_exported_sora_cam_image_cmd = "soracom sora-cam devices images"\
-        + " get-exported --device-id " + \
-        device_id + " --export-id " + export_id
+    get_exported_sora_cam_image_cmd = "soracom sora-cam devices images" \
+        + " get-exported --device-id " + device_id \
+        + " --export-id " + export_id
     updated_exported_image_info = json.loads(
         run_cmd(cmd=get_exported_sora_cam_image_cmd, arg=arg).stdout.decode())
     image_url = updated_exported_image_info.get("url")
